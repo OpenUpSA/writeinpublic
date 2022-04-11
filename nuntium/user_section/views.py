47c4c59fa25b92b7171f091881863171b12fc7c3
JD Bothma <jbothma@gmail.com>
2022-04-11 14:20:59 +0200

    Clarify reference to be a person, not a contact

--- nuntium/user_section/views.py
+++ nuntium/user_section/views.py
@@ -346,11 +346,11 @@ class MessagesPerWriteItInstance(LoginRequiredMixin, ListView, FormMixin):
     def post(self, request, *args, **kwargs):
 
         message_id = request.POST['message']
-        contact_ids = [int(id) for id in request.POST.getlist('contact_ids')]
-        outboundmessages = OutboundMessage.objects.filter(message_id = message_id, contact_id__in = contact_ids)
+        person_ids = [int(id) for id in request.POST.getlist('person_ids')]
+        outboundmessages = OutboundMessage.objects.filter(message_id = message_id, contact__person__in = person_ids)
 
         if outboundmessages.count() == 0:
-            return HttpResponseBadRequest("No matching outbound messages found for message {0} and contact IDs {1}".format(message_id, contact_ids))
+            return HttpResponseBadRequest("No matching outbound messages found for message {0} and person IDs {1}".format(message_id, person_ids))
  
         feedbackmessages = ''
 
