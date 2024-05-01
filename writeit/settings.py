5d3b7657322dec05bc130cd9c3bdef35cd5fe898
paulmwatson <github.com@paulmwatson.com>
2024-05-01 11:02:59 +0200

    Stop exceptions being sent as emails, app has Sentry

--- writeit/settings.py
+++ writeit/settings.py
@@ -282,11 +282,6 @@ LOGGING = {
         },
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
