90b9db92938032f62873bf6ca2690abd588d9b8d
michaelglenister <mickeyjhg@gmail.com>
2026-02-17 19:39:12 +0200

    Py3 compatability fixes

--- writeit/compat.py
+++ writeit/compat.py
@@ -1,5 +1,18 @@
-# Compatibility shim for django-plugins which imports django.utils.six
-# (removed in Django 3.0). This must be imported before djangoplugins.
+# Compatibility shim for django-plugins which imports modules
 import django.utils
+import django.utils.encoding
+import django.conf.urls
+import django.core.management.base
 import six
 django.utils.six = six
+
+django.utils.encoding.python_2_unicode_compatible = lambda cls: cls
+
+def _compat_patterns(prefix, *args):
+    if prefix:
+        raise Exception("Using a prefix with patterns() is not supported.")
+    return list(args)
+
+django.conf.urls.patterns = _compat_patterns
+
+django.core.management.base.NoArgsCommand = django.core.management.base.BaseCommand
